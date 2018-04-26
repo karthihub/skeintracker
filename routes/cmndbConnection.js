e3fa3d0b5ef30692f9b900779e24a8236e375736
karthihub <karthiikeyansekar@gmail.com>
2018-04-26 23:37:04 +0530

    Initial commit

--- routes/cmndbConnection.js
+++ routes/cmndbConnection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: 'localhost',
+    host: "https://www.skeinlab.com",
 //    port: 8081,
     user: 'root',
-    password: 'root@123',
+    password: '',
     database: 'sample'
 });
 
