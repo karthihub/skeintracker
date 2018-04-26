1f5b2e06b4c8340b2d6ae164f756ad356e254f77
karthihub <karthiikeyansekar@gmail.com>
2018-04-27 00:11:54 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
-var port = process.env.port || 8989;
+var port = process.env.port || 3000;
 
 //set view engin part
 app.set('view engine', 'ejs');
@@ -13,33 +13,53 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(bodyParser());
 
-//Task Routes
-var routescmnAddTask = require('./routes/cmnAddTask');
-app.use(routescmnAddTask);
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'https://www.skeinlab.com',
+  // port     : 'port',
+  user     : 'root',
+  password : '',
+  database : 'sample'
+});
 
-//Authendication Routes
-var routescmnAuth = require('./routes/cmnAuth');
-app.use(routescmnAuth);
+connection.connect();
+var b;
+console.log(query);
+connection.query(query, function(err, rows, fields) {
+  if (err) throw err;
+   b = rows;
+   console.log(b);
+   console.log(rows);
+   result(b);
+});
 
-//Leave Routes
-var routescmnLeave = require('./routes/cmnLeave');
-app.use(routescmnLeave);
+// //Task Routes
+// var routescmnAddTask = require('./routes/cmnAddTask');
+// app.use(routescmnAddTask);
 
-//Managers Routes
-var routescmnMngrRoutes = require('./routes/cmnMngrRoutes');
-app.use(routescmnMngrRoutes);
+// //Authendication Routes
+// var routescmnAuth = require('./routes/cmnAuth');
+// app.use(routescmnAuth);
 
-//Project Routes
-var routescmnProject = require('./routes/cmnProject');
-app.use(routescmnProject);
+// //Leave Routes
+// var routescmnLeave = require('./routes/cmnLeave');
+// app.use(routescmnLeave);
 
-//User Details Routes
-var routescmnRegistration = require('./routes/cmnRegistration');
-app.use(routescmnRegistration);
+// //Managers Routes
+// var routescmnMngrRoutes = require('./routes/cmnMngrRoutes');
+// app.use(routescmnMngrRoutes);
 
-//Notifications Routes
-var routescmnNotifications = require('./routes/cmnNotification');
-app.use(routescmnNotifications);
+// //Project Routes
+// var routescmnProject = require('./routes/cmnProject');
+// app.use(routescmnProject);
+
+// //User Details Routes
+// var routescmnRegistration = require('./routes/cmnRegistration');
+// app.use(routescmnRegistration);
+
+// //Notifications Routes
+// var routescmnNotifications = require('./routes/cmnNotification');
+// app.use(routescmnNotifications);
 
 // app.get('*', function(req, res){
 //   res.send(405, 'Menthod not allowed');
