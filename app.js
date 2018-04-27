5fd700dfa16d0f692b6cfb778542699cbfd997ef
karthihub <karthiikeyansekar@gmail.com>
2018-04-27 11:19:24 +0530

    Initial commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
 var app = express();
-var port = process.env.port;
+var port = process.env.port || 3000;
 
 //set view engin part
 app.set('view engine', 'ejs');
