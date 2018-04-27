c70b1bf65891dcb04848bc9786d6c6b1c46b501a
karthihub <karthiikeyansekar@gmail.com>
2018-04-27 11:34:27 +0530

    Initial commit

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var mysql = require('mysql');
  var connection = mysql.createConnection({
   host     : '127.0.0.1',
   port     : '3306',
-  user     : 'userPBI',
-  password : 'cUsyE0b718XpKwRL',
+  user     : 'userOEW',
+  password : 'SuyaukiwCFNYnwQW',
   database : 'sampledb'
  });
 
