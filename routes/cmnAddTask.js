944ab65c02e1fa7941229ee84052e4983a6cc5b9
karthihub <karthiikeyansekar@gmail.com>
2018-05-02 18:00:14 +0530

    Initial commit

--- routes/cmnAddTask.js
+++ routes/cmnAddTask.js
@@ -4,26 +4,53 @@ var mysql = require('mysql');
 var dbConnection = require('./cmndbConnection');
 var responceFile = require('../public/javascripts/responceFile');
 // var sendMail = require('../public/javascripts/maillerFunction');
+var moment = require('moment');
 
 
 router.post('/empTaskAdd', function(req, res){
-    var skeinID = req.body.skein_id;
+    var skeinID = (req.body.skein_id)?req.body.skein_id:"";
+    var assignedBy = (req.body.assignedBy)?req.body.assignedBy:"";
     var projectname = req.body.projectname;
     var taskname = req.body.taskname;
     var timetaken = req.body.timetaken;
     var status = req.body.status;
     var date = req.body.date;
     var description = req.body.description;
+    var query = "";
 
-    dbConnection.query("INSERT INTO addtask (skein_id,projectname,taskname,timetaken,status,date,description) values ('"+skeinID+"','"+projectname+"','"+taskname+"','"+timetaken+"','"+status+"','"+date+"','"+description+"')", function (err, result, fields) {
+    if(skeinID != "" && assignedBy != ""){
+        var query = "INSERT INTO addtask (skein_id,projectname,taskname,timetaken,status,date,description,assignedBy) values ('"+skeinID+"','"+projectname+"','"+taskname+"','"+timetaken+"','"+status+"','"+date+"','"+description+"','"+assignedBy+"')";
+    }else if(skeinID == "" && assignedBy != ""){
+        var query = "INSERT INTO addtask (skein_id,projectname,taskname,timetaken,status,date,description,assignedBy) values ('"+skeinID+"','"+projectname+"','"+taskname+"','"+timetaken+"','"+status+"','"+date+"','"+description+"','')";
+    }
+
+    dbConnection.query(query, function (err, result, fields) {
         if (err){
             responceFile.status = 401;
             responceFile.message = "Database Error, Please try again";
             res.send(responceFile);
         }else if(result){
+
+            if(skeinID != "" && assignedBy != ""){
+            dbConnection.query("UPDATE skeinbook SET assignedBy='"+assignedBy+"',assignedDate='"+ moment().format("YYYY-MM-DD") +"' where skein_id='"+skeinID+"'", function (err, result, fields) {
+                if (err){
+                    responceFile.status = 401;
+                    responceFile.message = "Database Error, Please try again";
+                    res.send(responceFile);
+                }else if(result){
+                    responceFile.status = 200;
+                    responceFile.message = "User "+skeinID+" task added successfully";
+                    res.send(responceFile);
+                  }
+                
+            });
+        }else{
             responceFile.status = 200;
             responceFile.message = "User "+skeinID+" task added successfully";
             res.send(responceFile);
+        }
+
+            
           }
         
     });
